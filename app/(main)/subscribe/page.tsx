96411d05eba130b41c1873860439f7c3e689601b
Tanvir Ahamed <142485438+Tanvir286@users.noreply.github.com>
2026-02-23 14:38:53 +0600

    Update page.tsx

--- app/(main)/subscribe/page.tsx
+++ app/(main)/subscribe/page.tsx
@@ -13,25 +13,4 @@ export default function SubscribePage() {
     </div>
   );
 }
-// "use client";
 
-// import { useSearchParams } from "next/navigation";
-// import SubscriptionCheckout from "@/components/homePage/subscribe/Subscription-cehckout";
-
-// export default function SubscribePage() {
-//   const searchParams = useSearchParams();
-
-//   const price = Number(searchParams.get("price"));
-//   const plan = searchParams.get("plan");
-
-//   // Optional safety check
-//   if (!price || !plan) {
-//     return <p className='p-10'>Invalid subscription selection</p>;
-//   }
-
-//   return (
-//     <div>
-//       <SubscriptionCheckout price={price} plan={plan} />
-//     </div>
-//   );
-// }
