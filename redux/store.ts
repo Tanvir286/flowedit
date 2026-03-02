3134ab3dbd5b6f4d27ade72872afc1a1f2f361f9
Tanvir Ahamed <142485438+Tanvir286@users.noreply.github.com>
2026-03-02 14:29:20 +0600

    Update store.ts

--- redux/store.ts
+++ redux/store.ts
@@ -14,17 +14,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-
-// import { configureStore } from "@reduxjs/toolkit";
-// import { baseApi } from "./api/baseApi";
-// import authReducer from "./features/auth/authSlice";
-
-// export const store = configureStore({
-//   reducer: {
-//     [baseApi.reducerPath]: baseApi.reducer,
-//     auth: authReducer,
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware().concat(baseApi.middleware),
-// });
